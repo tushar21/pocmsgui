85b0e5adb572f7a54886f036cacb5281be2b9868
tusharnikam1 <tushar.nikam@globant.com>
2019-01-26 19:27:18 +0530

    Update server api hostname

--- src/services/data.js
+++ src/services/data.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default {
   search (q = { }) {
-    return axios.get('http://localhost:3000/pomsg/all?item=' + q.itemcode + '&warehouse=' + q.warehouse)
+    return axios.get('http://pocmsgapi:3001/pomsg/all?item=' + q.itemcode + '&warehouse=' + q.warehouse)
   }
 }
